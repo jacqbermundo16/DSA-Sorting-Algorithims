f8f6be464da686c7a56d7adb5a0fc4885cf886db
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-01-27 17:32:47 +0800

    This program implements the insertion sort algorithm, wherein the program considers the first element as a part of a sorted array and the rest to be unsorted and one by one, it will include each of the elements, by storing the next elemnt in a variable to leave a space that will be added to the sorted array. Once the space is added, the program will then move the space so that all the elements larger than the value of the key will move to the right. The program will implement this method until all the elements are included and sorted accordingly.

---
+++ insertionsort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        j = i 
+        while arr[j-1] > arr[j] and j > 0:
+            arr[j-1], arr[j] = arr[j], arr[j-1]
+            j -= 1
+        print(arr)
+
+arr = [39, 77, 46, 23, 94, 3, 98, 19, 62, 2]
+insertion_sort(arr)
+print(arr)
