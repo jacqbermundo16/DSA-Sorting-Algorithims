4f895b2c4f8041e5c90d90cb8b50a855a2b40fd2
Jacqueline C Bermundo <92629695+jacqbermundo16@users.noreply.github.com>
2023-01-27 16:54:32 +0800

    This program implements the selection sort algorithm, where in the program searches for the smallest value in the array and swap its position from where it started the seach until all the elements are sorted.

---
+++ selectionsort.py
@@ -0,0 +1,16 @@
+def sort(nums):
+    for i in range(9):
+        minpos = i
+        for j in range(i, 10):
+            if nums[j] < nums[minpos]:
+                minpos = j
+                
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+        print(nums)
+
+
+nums = [39, 77, 46, 23, 94, 3, 98, 19, 62, 2]
+sort(nums)
+print(nums)
